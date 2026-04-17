10e4954273ab512e18410f9125ef7c00fd8eb530
Simon Mok <simon214d@gmail.com>
2026-04-17 20:06:10 +0200

    css cleanup and add cool background 😎

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import youtubeLogo from './assets/youtube.png'
 import githubLogo from './assets/github.png'
 import dummyDashboard from './assets/dummy_dashboard.png'
 import banana from './assets/peanutbutter_jelly_time.png'
+import backgroundImage from './assets/mokznet_star_sky_1.png'
 import './App.css'
 import Navbar from './Navbar';
 
@@ -13,40 +14,37 @@ function App() {
     <>
       <Navbar />
       <section id="about_and_meme">
-        <div id="docs">
+        <div id="about">
           <h2>About</h2>
           <p>Hello i am Simon :=) 💪😻💪</p>
           <ul>
             <li>
-              <a href="https://www.youtube.com/@mokz99" target="_blank">
+              <a href="https://www.youtube.com/@mokz99" target="_blank" className="hover-effect">
                 <img src={youtubeLogo} alt="YouTube" style={{ height: '1em', width: 'auto' }}/>
-                Youtube
+                YouTube
               </a>
             </li>
             <li>
-              <a href="https://github.com/mokz99" target="_blank">
+              <a href="https://github.com/mokz99" target="_blank" className="hover-effect">
                 <img src={githubLogo} alt="GitHub" style={{ height: '1em', width: 'auto' }}/>
                 GitHub
               </a>
             </li>
           </ul>
         </div>
-        <div id="social">
+        <div id="meme">
           <h2>Funi meme</h2>
           <img src={banana} alt="Dashboard" style={{ height: '10em', width: 'auto' }}/>
         </div>
       </section>
       
-
       <section id="spacer"></section>
-
       <section id="dashboard">
         <div>
           <h2>Server Dashboard</h2>
           <img src={dummyDashboard} alt="Dashboard" style={{ height: '20em', width: 'auto' }}/>
         </div>
       </section>
-
       <section id="spacer"></section>
     </>
   )
